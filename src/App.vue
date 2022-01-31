f7efbe304b23abddc9ca1419ae2063afc385299a
Denis Kravchuk <deniskrv@gmail.com>
2022-01-31 22:11:21 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app">
+    <post-form @create="createPost"></post-form>
+    <post-list :posts="posts" @remove="removePost"></post-list>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+
+export default {
+  components: {
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
+        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 15px;
+}
+</style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "my-button",
+};
+</script>
+
+<style scoped>
+.btn {
+  /* margin-top: 15px; */
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  cursor: pointer;
+  border: 1px solid teal;
+  transition: 0.5s;
+}
+.btn:hover {
+  background: teal;
+  color: white;
+}
+</style>
